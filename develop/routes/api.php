6ab6520b84e8589d557a4dcfb6f292f468c8274b
tech <tech.yourshealth@gmail.com>
2023-04-17 23:08:34 +0200

    setup linebot callback

--- develop/routes/api.php
+++ develop/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['namespace' => 'Api'], function() {
+    Route::post('/callback', 'LineWebhookController@webhook')->name('line.webhook');
+});
