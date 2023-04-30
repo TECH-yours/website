7e3c5d934cba87eec189b35f0964e351d94fa131
tech <tech.yourshealth@gmail.com>
2023-04-30 05:06:03 +0200

    use .env and logged the response

--- develop/routes/api.php
+++ develop/routes/api.php
@@ -24,9 +24,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::any('/callback', function(Request $request){
 
-    $ChannelAccessToken ='QhIrTjbp4Y2TxAWVLYNZmFETOPC/R6r7utvGKBcMqWCZZok+sB/E/plPr/nobz6ww2AsRYrXGc3NRGeSO3sqEeYh45HCXUW7OzD8IRkgMXywUQZWJJE4qtz1UJSqxJpJFfNkZhA0qYQbrVXTq3NrugdB04t89/1O/w1cDnyilFU=';
-    $ChannelUserId = '1660879908';
-    $ChannelSecret = '06781fc855c82ce0cb9c8552e09a742c';
+    $ChannelAccessToken = env('LINE_CHANNEL_ACCESS_TOKEN'); //'QhIrTjbp4Y2TxAWVLYNZmFETOPC/R6r7utvGKBcMqWCZZok+sB/E/plPr/nobz6ww2AsRYrXGc3NRGeSO3sqEeYh45HCXUW7OzD8IRkgMXywUQZWJJE4qtz1UJSqxJpJFfNkZhA0qYQbrVXTq3NrugdB04t89/1O/w1cDnyilFU=';
+    $ChannelUserId = env('LINE_CHANNEL_USERID'); //'1660879908';
+    $ChannelSecret = env('LINE_CHANNEL_SECRET'); //'06781fc855c82ce0cb9c8552e09a742c';
 
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($ChannelAccessToken);
     $bot = new \LINE\LINEBot($httpClient,['channelSecret' => $ChannelSecret]);
@@ -43,20 +43,20 @@ Route::any('/callback', function(Request $request){
             $client = \Illuminate\Support\Facades\Http::withHeaders([
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer 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'
+                'Authorization' => env('CHATGPT_TOKEN')
             ]);
         
-            $response = $client->post('https://api.openai.com/v1/chat/completions', [
+            $response = $client->post('https://api.openai.com/v1/chat/completions', 
+            [
                 'model' => "gpt-3.5-turbo-0301",
-                "messages" => [
-                    [
-                        "role" => "user", 
-                        "content" => "how many water should a person drinks everyday"
-                    ]
+                "messages" => 
+                [
+                    [ "role" => "user", "content" => $text ]
                 ]
             ]);
             
             $response_json = json_decode($response, true);
+            Log::info($response_json);
             $chatgpt = $response_json['choices'][0]['message']['content'];
             
             $bot->replyText($reply_token, $chatgpt);
@@ -68,7 +68,7 @@ Route::get('/test', function(){
     $client = \Illuminate\Support\Facades\Http::withHeaders([
         'Accept' => 'application/json',
         'Content-Type' => 'application/json',
-        'Authorization' => 'Bearer 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'
+        'Authorization' => env('CHATGPT_TOKEN')
     ]);
 
     $response = $client->post('https://api.openai.com/v1/chat/completions', [
