2e58242e9b1189becb3abee5ca8bd6d1e2e64b1b
tech <tech.yourshealth@gmail.com>
2024-03-21 16:46:42 +0100

    -

--- develop/app/Models/Restaurant.php
+++ develop/app/Models/Restaurant.php
@@ -67,7 +67,7 @@ class Restaurant extends Model
     // get all
     public static function getAll()
     {
-        $restaurants = Restaurant::get()->orderBy('restaurant.id', 'desc');
+        $restaurants = Restaurant::orderBy('restaurant.id', 'desc')->get();
         foreach ($restaurants as $restaurant) {
             $restaurant->meals = Meals::where('rid', $restaurant->id)
                 ->join('restaurant', 'meals.rid', '=', 'restaurant.id')
