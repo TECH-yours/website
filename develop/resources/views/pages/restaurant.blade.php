3ba5e6e3ac688c43c227a4f033b204761e8be88b
tech <tech.yourshealth@gmail.com>
2024-03-21 13:00:33 +0100

    update restaurants list render

--- develop/resources/views/pages/restaurant.blade.php
+++ develop/resources/views/pages/restaurant.blade.php
@@ -1,20 +1,15 @@
 @include('includes.language')
-@extends('layouts.default', ['page_header' =>'餐廳列表','page_parent' =>'Home','page_parent_path' =>'/','page_path' =>'restaurant'])
+@extends('layouts.default', ['page_header' =>'餐廳列表','page_parent' =>'Home','page_parent_path' =>'/','keyword' =>''])
 @section('content')
 
 
-<div id="breadcrumb" class="breadcrumb-area bg-img" style="background-image: url(/img/cover.jpg);">
+<div id="breadcrumb" class="breadcrumb-area bg-img" style="background-image: url(/images/meals-table.png);">
     <div class="container h-100">
         <div class="row h-100 align-items-center">
             <div class="col-12">
-                <div class="breadcrumb-content text-center">
-                    <h2 class="page-title">餐廳列表</h2>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb justify-content-center">
-                            <li class="breadcrumb-item"><a href="/">HOME</a></li>
-                            <li class="breadcrumb-item active breadcrumb-item-active" aria-current="page">Restaurant</li>
-                        </ol>
-                    </nav>
+                <div class="breadcrumb-content">
+                    <h2>全台餐廳懶人包</h2>
+                    <h4>本站所有品項整合POS 系統</h4>
                 </div>
             </div>
         </div>
@@ -22,21 +17,138 @@
 </div>
 
 <div class="container">
-    <div class="d-flex flex-wrap">
-    @foreach ($restaurants as $restaurant)
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3" style="padding: 1rem;">
-        <div class="card">
-            <img class="card-img-top" src="/images/{{ $restaurant->thumbnailImageUrl }}" alt="Restaurants thumbnail" style="height: 180px; object-fit: contain;">
-            <div class="card-body">
-                <h5 class="card-title mt-1">{{ $restaurant->name }}</h5>
-                <p class="card-text">{{ $restaurant->address }}</p>
-                <a href="/restaurant/{{ $restaurant->id }}/menu" class="btn btn-primary">查看菜單</a>
-                <a href="{{ $restaurant->google_map_url }}" target="_blank" class="btn btn-light"><i class="fas fa-map"></i></a>
+
+    <!--  Search Form -->
+    <form action="./" method="GET">
+        <div class="py-3 d-flex flex-wrap">
+            <div class="col-12">
+                <div class="input-group mb-1">
+                    <input type="text" class="form-control" placeholder="取餐位置" name="location">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit">自動定位</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 mb-3">
+                <div class="input-group">
+                    <select class="custom-select" name="category">
+                        <option value="">請選擇餐廳類別</option>
+                        @foreach ($categories as $category)
+                        <option value="{{ $category }}">{{ $category }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="請輸入關鍵字" name="keyword">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit">搜尋</button>
+                    </div>
+                </div>
             </div>
         </div>
+    </form>
+
+
+    <h2 class="section-heading">新進品牌</h2>
+    <div class="horizontal-scroll-wrapper">
+        @foreach ($restaurants as $restaurant)
+        <div class="col-7 col-lg-4" style="padding: 1rem;">
+            <div class="card" onclick="location.href='/restaurant/{{ $restaurant->id }}/menu';" style="cursor: pointer;">
+                <img class="card-img-top" src="/images/{{ $restaurant->thumbnailImageUrl }}" alt="Restaurants thumbnail" style="height: 180px; object-fit: contain;">
+                <div class="like-btn {{ rand(0, 1) ? 'active' : '' }}">
+                    <i class="far fa-heart"></i>
+                    <i class="fas fa-heart" style="color: #ff0000;"></i>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title mt-1">{{ $restaurant->name }}</h5>
+                    <div class="tag-group">
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-utensils"></i></div>
+                            <div class="tag-text">健康餐</div>
+                        </div>
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-dollar-sign"></i></div>
+                            <div class="tag-text">200-400</div>
+                        </div>
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-motorcycle"></i></div>
+                            <div class="tag-text">滿 500 免運</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
-    @endforeach
+
+
+    <h2 class="section-heading">猜你喜歡</h2>
+    <div class="horizontal-scroll-wrapper">
+        @foreach ($restaurants as $restaurant)
+        <div class="col-7 col-lg-4" style="padding: 1rem;">
+            <div class="card" onclick="location.href='/restaurant/{{ $restaurant->id }}/menu';" style="cursor: pointer;">
+                <img class="card-img-top" src="/images/{{ $restaurant->thumbnailImageUrl }}" alt="Restaurants thumbnail" style="height: 180px; object-fit: contain;">
+                <div class="like-btn {{ rand(0, 1) ? 'active' : '' }}">
+                    <i class="far fa-heart"></i>
+                    <i class="fas fa-heart" style="color: #ff0000;"></i>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title mt-1">{{ $restaurant->name }}</h5>
+                    <div class="tag-group">
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-utensils"></i></div>
+                            <div class="tag-text">健康餐</div>
+                        </div>
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-dollar-sign"></i></div>
+                            <div class="tag-text">200-400</div>
+                        </div>
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-motorcycle"></i></div>
+                            <div class="tag-text">滿 500 免運</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
+
+    <h2 class="section-heading">所有餐廳</h2>
+    <div class="d-flex flex-wrap">
+        @foreach ($restaurants as $restaurant)
+        <div class="col-12 col-md-6" style="padding: 1rem;">
+            <div class="card" onclick="location.href='/restaurant/{{ $restaurant->id }}/menu';" style="cursor: pointer;">
+                <img class="card-img-top" src="/images/{{ $restaurant->thumbnailImageUrl }}" alt="Restaurants thumbnail" style="height: 180px; object-fit: contain;">
+                <div class="like-btn {{ rand(0, 1) ? 'active' : '' }}">
+                    <i class="far fa-heart"></i>
+                    <i class="fas fa-heart" style="color: #ff0000;"></i>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title mt-1">{{ $restaurant->name }}</h5>
+                    <div class="tag-group">
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-utensils"></i></div>
+                            <div class="tag-text">健康餐</div>
+                        </div>
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-dollar-sign"></i></div>
+                            <div class="tag-text">200-400</div>
+                        </div>
+                        <div class="tag">
+                            <div class="tag-icon"><i class="fas fa-motorcycle"></i></div>
+                            <div class="tag-text">滿 500 免運</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+
+
 </div>
 
 @stop
--- develop/routes/web.php
+++ develop/routes/web.php
@@ -19,7 +19,10 @@ use App\Models\Restaurant;
 // });
 
 Route::get('/', function () {
-    return view('pages.index');
+    $restaurants = Restaurant::getAll();
+    $categories = ['中式料理', '日式料理', '美式料理', '義式料理', '韓式料理', '泰式料理', '素食料理', '其他'];
+    return view('pages.restaurant', ['restaurants' => $restaurants, 'categories' => $categories]);
+    // return view('pages.index');
 });
 
 Route::get('/restaurants', function () {
