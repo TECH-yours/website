bd0986d90e746a2e7436c80e5d9c0f44ee300c1e
tech <tech.yourshealth@gmail.com>
2024-03-21 16:16:56 +0100

    clean up

--- develop/resources/views/includes/head.blade.php
+++ develop/resources/views/includes/head.blade.php
@@ -12,12 +12,12 @@
 <link rel="stylesheet" href="/css/info.css">
 <!-- <link rel="stylesheet" href="/css/info_500.css"> -->
 
-<link rel="stylesheet" href="/libs/owl-carousel/docs.theme.min.css">
+<!-- <link rel="stylesheet" href="/libs/owl-carousel/docs.theme.min.css">
 <link rel="stylesheet" href="/libs/owl-carousel/owl.carousel.min.css">
-<link rel="stylesheet" href="/libs/owl-carousel/owl.theme.default.min.css">
+<link rel="stylesheet" href="/libs/owl-carousel/owl.theme.default.min.css"> -->
 
 <!-- jquery -->
-<script src="/libs/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+<script src="/libs/jquery-3.6.4.js"></script>
 
 <!-- swiper -->
 <script src="/libs/swiper@8/swiper-bundle.min.js"></script>
@@ -27,13 +27,13 @@
 <script src="/libs/jquery.growl.min.js"></script>
 
 <!-- bootstrap -->
-<script src="/libs/bootstrap@4.6.2/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="/libs/bootstrap@4.6.2/bootstrap.bundle.min.js"></script>
 
 <!-- bootstrap-select -->
 <script src="/bootstrap-select@1.13.14/bootstrap-select.min.js"></script>
 
-<script src="/libs/wow.min.js"></script>
-<script src="/libs/owl-carousel/owl.carousel.js"></script>
+<!-- <script src="/libs/wow.min.js"></script> -->
+<!-- <script src="/libs/owl-carousel/owl.carousel.js"></script> -->
 
 <!-- moment -->
 <script src="/libs/moment.min.js"></script>
